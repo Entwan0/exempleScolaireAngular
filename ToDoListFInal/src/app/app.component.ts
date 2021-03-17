0aabc157af5bd3f131ef5738aa79c570afa6f05a
l3miage-volozana <74137436+l3miage-volozana@users.noreply.github.com>
2021-03-17 10:24:44 +0100

    Add files via upload

---
+++ ToDoListFInal/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+
+  constructor() {
+    /* Ajoutez un paramètre de type TodolistService au constructeur */
+  }
+
+}
